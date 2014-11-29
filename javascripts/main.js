eb51943751b5968fd88694218e4bb4bce071c76c
Dinh Cong Thang <dinhcongthang113@hotmail.com>
2014-11-29 12:08:19 +0700

    Init

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log('This would be the main JS file.');
+$(function () {
+	$('#tblEmailExtractor').dataTable({
+		"dom": 'T<"clear">lfrtip',
+		"tableTools": {
+			"sSwfPath": "/content/copy_csv_xls_pdf.swf"
+		}
+	});
+	
+	$('#tabs').tab();
+
+	var mainUrl = 'http://vietsoftapi.apphb.com/api/emailMarketing';
+	$('#btnExtractEmail').click(function () {
+		$('.wait').show();
+		$.post(mainUrl, { type: 1, content: $('#tbxCopyPasteContainer').val(), url: '' }, function (data) {
+			$('#divScripts').html(data);
+		});
+	});
+
+	$('#btnExtractEmailFromUrl').click(function () {
+		$('.wait').show();
+		$.post(mainUrl, { type: 3, content: '', url: $('#tbxUrl').val() }, function (data) {
+			$('#divScripts').html(data);
+		});
+	});
+});
